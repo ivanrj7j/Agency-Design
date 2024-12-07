816c58e0ee36655116551e97be344fdf2ecaba2a
Sushant98K <sushant98k@gmail.com>
2024-12-07 19:55:00 +0530

    Page 3 Compelte

--- script.js
+++ script.js
@@ -39,8 +39,6 @@ function navAnimation() {
   });
 }
 
-navAnimation();
-
 function page2Animation() {
   var rightElems = document.querySelectorAll(".right-elem");
 
@@ -66,4 +64,36 @@ function page2Animation() {
   });
 }
 
+function page3VideAnimation() {
+  var page3Center = document.querySelector(".page3-center");
+  var video = document.querySelector("#page3 video");
+
+  page3Center.addEventListener("click", function () {
+    video.play();
+    gsap.to(video, {
+      transform: "scaleX(1) scaleY(1)",
+      opacity: 1,
+      borderRadius: 0,
+    });
+  });
+
+  video.addEventListener("click", function () {
+    video.pause();
+    gsap.to(video, {
+      transform: "scaleX(0.7) scaleY(0)",
+      opacity: 0,
+      borderRadius: "30px",
+    });
+  });
+
+}
+
+
+
+
+
+
+
+navAnimation();
 page2Animation();
+page3VideAnimation();
